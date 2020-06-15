feb1709db1da43150dada7fc6b36e69846865623
Anthony Stiven Luque <kasuru@hotmail.com>
2020-06-15 09:47:35 -0500

    add jorge tercer intento

--- prueba_travis/urls.py
+++ prueba_travis/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from colombia import views as views_colombia
+from django.http import HttpResponse 
 from django.urls import path
 
 def hola(request):
